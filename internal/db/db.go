6fccb85050a3497e28787282c930ba9070692794
nayu104 <ose794@gmail.com>
2025-12-18 00:14:55 +0900

    add database connection setup and Memo struct definition

---
+++ internal/db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+// %wとは。。。
+func New() (*sql.DB, error) {
+	dsn := os.Getenv("DATABASE_URL")
+
+	 // 2) 環境変数が空なら、そもそも接続先が分からないのでエラーにする
+    if dsn == "" {
+        return nil, fmt.Errorf("DATABASE_URL is not set")
+    }
+
+	conn, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open failed: %w", err)
+	}
+
+	if err := conn.Ping(); err != nil {
+		_ = conn.Close()
+		return nil, fmt.Errorf("db.Ping failed: %w", err)
+	}
+	return conn, nil
+}
